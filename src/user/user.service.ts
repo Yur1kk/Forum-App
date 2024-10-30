cc0694e40e7f102762919076d5a32c041e437aeb
Yurii Soroka <urasor04@gmail.com>
2024-10-30 18:21:49 +0200

    feat: provided functionality that allows to operate with users

---
+++ src/user/user.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+import * as bcrypt from 'bcrypt';
+import { RegisterDto } from '../auth/dto/register.dto'; 
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async createUser(data: RegisterDto) {
+    const hashedPassword = await bcrypt.hash(data.password, 10);
+    return this.prisma.user.create({
+      data: { 
+        ...data, 
+        password: hashedPassword,
+        role: { connect: { id: 1 } } 
+      },
+    });
+  }
+
+  async findUserByEmail(email: string) {
+    return this.prisma.user.findUnique({ where: { email } });
+  }
+
+  async updatePassword(email: string, newPassword: string) {
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+    await this.prisma.user.update({
+      where: { email },
+      data: { password: hashedPassword, resetToken: null }, 
+    });
+  }
+
+  async saveResetToken(userId: number, token: string) {
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { resetToken: token }, 
+    });
+  }
+
+  async findUserByResetToken(token: string) {
+    const users = await this.prisma.user.findMany(); 
+    return users.find(user => user.resetToken === token); 
+  }
+  
+
+  async clearResetToken(userId: number) {
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { resetToken: null }, 
+    });
+  }
+}
