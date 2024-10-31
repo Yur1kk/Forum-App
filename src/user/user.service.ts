a035288ec3ef752cd171115e90df8dcd3e85973c
Yurii Soroka <urasor04@gmail.com>
2024-10-31 18:05:00 +0200

    feat: provided functionality for adding, deleting and updating user profile photo

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException} from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import * as bcrypt from 'bcrypt';
 import { RegisterDto } from '../auth/dto/register.dto'; 
+import { ProfilePhotoDto } from './dto/profile-photo.dto';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: RegisterDto) {
-    const { confirmPassword, ...userData } = data;
     const hashedPassword = await bcrypt.hash(data.password, 10);
     return this.prisma.user.create({
       data: { 
@@ -53,5 +53,54 @@ export class UserService {
       where: { id: userId },
       data: { resetToken: null }, 
     });
+  }
+
+  async addUserPhoto(userId: number, profilePhotoDto: ProfilePhotoDto) {
+    const user = await this.findUserById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const profilePhoto = user.profilePhoto
+    if (profilePhoto) {
+      throw new BadRequestException('Profile photo exists already');
+    }
+    await this.prisma.user.update({
+      where: {id: userId},
+      data: {
+        profilePhoto: profilePhotoDto.profilePhoto,
+      },
+    });
+    return {message: 'Profile photo has been added succesfully!'};
+  }
+  async deleteUserPhoto(userId: number) {
+    const user = await this.findUserById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (!user.profilePhoto) {
+      throw new NotFoundException('User does not have a profile photo');
+  }
+
+   await this.prisma.user.update({
+    where: {id: userId},
+    data: {profilePhoto: null},
+   });
+   return {message: 'Profile photo has been deleted succesfully!'};
+  }
+
+  async updateUserPhoto(userId: number, profilePhotoDto: ProfilePhotoDto) {
+    const user = await this.findUserById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    if (!user.profilePhoto) {
+      throw new NotFoundException('User does not have a profile photo');
+  }
+   const profilePhoto = user.profilePhoto;
+   await this.prisma.user.update({
+    where: {id: userId},
+    data: {profilePhoto: profilePhotoDto.profilePhoto ?? profilePhoto},
+   });
+   return {message: 'Profile photo has been updated succesfully!'};
   }
 }
