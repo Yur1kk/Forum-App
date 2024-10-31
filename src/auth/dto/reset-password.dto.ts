7eeb9c054b841ddd2456720fbeaeb6c764671569
Yurii Soroka <urasor04@gmail.com>
2024-10-31 17:25:13 +0200

    fix: changed response to english

--- src/auth/dto/reset-password.dto.ts
+++ src/auth/dto/reset-password.dto.ts
@@ -3,9 +3,17 @@ import { IsString, MinLength, Matches } from 'class-validator';
 export class ResetPasswordDto {
   @IsString()
   @MinLength(6)
-  @Matches(/(?=.*[0-9])/, { message: 'Пароль повинен містити принаймні одну цифру' })
-  @Matches(/(?=.*[A-Z])/, { message: 'Пароль повинен містити принаймні одну велику літеру' })
-  @Matches(/(?=.*[a-z])/, { message: 'Пароль повинен містити принаймні одну малу літеру' })
-  @Matches(/(?=.*[!@#$%^&*])/, { message: 'Пароль повинен містити принаймні один спеціальний символ' })
+  @Matches(/(?=.*[0-9])/, { message: 'Password must contain at least one number' })
+  @Matches(/(?=.*[A-Z])/, { message: 'Password must contain at least one capital letter' })
+  @Matches(/(?=.*[a-z])/, { message: 'Password must contain at least one small letter' })
+  @Matches(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special symbol' })
   newPassword: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/(?=.*[0-9])/, { message: 'Password must contain at least one number' })
+  @Matches(/(?=.*[A-Z])/, { message: 'Password must contain at least one capital letter' })
+  @Matches(/(?=.*[a-z])/, { message: 'Password must contain at least one small letter' })
+  @Matches(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special symbol' })
+  confirmNewPassword: string;
 }
