79568ae6bae997df929c8c20db0b13ea7facfdb7
Yurii Soroka <urasor04@gmail.com>
2024-11-01 14:08:18 +0200

    Connected admin module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module'; 
 import { PrismaService } from 'prisma/prisma.service';
 import { PostsModule } from './posts/posts.module';
+import { AdminModule } from './auth/admin/admin.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PostsModule],
+  imports: [AuthModule, UserModule, PostsModule, AdminModule],
   providers: [PrismaService],
 })
 export class AppModule {}
