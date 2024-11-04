c70e8205ded717bca2c25ce929f4ed6844155b9a
Yurii Soroka <urasor04@gmail.com>
2024-11-04 17:35:25 +0200

    fix: added missing validation and user-name field

--- src/auth/admin/dto/admin-register.dto.ts
+++ src/auth/admin/dto/admin-register.dto.ts
@@ -1,15 +1,30 @@
-import { IsEmail, IsString, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsString, IsNotEmpty, Matches, MinLength, IsOptional } from 'class-validator';
 
 export class RegisterAdminDto {
     @IsEmail()
     email: string;
 
     @IsString()
-    @IsNotEmpty()
+    @MinLength(6)
+    @Matches(/(?=.*[0-9])/, { message: 'Password must contain at least one number' })
+    @Matches(/(?=.*[A-Z])/, { message: 'Password must contain at least one capital letter' })
+    @Matches(/(?=.*[a-z])/, { message: 'Password must contain at least one small letter' })
+    @Matches(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special symbol' })
     password: string;
 
     @IsString()
-    @IsNotEmpty()
+    name?: string;
+  
+    @IsString()
+    @IsOptional()
+    profilePhoto?: string;
+
+    @IsString()
+    @MinLength(6)
+    @Matches(/(?=.*[0-9])/, { message: 'Password must contain at least one number' })
+    @Matches(/(?=.*[A-Z])/, { message: 'Password must contain at least one capital letter' })
+    @Matches(/(?=.*[a-z])/, { message: 'Password must contain at least one small letter' })
+    @Matches(/(?=.*[!@#$%^&*])/, { message: 'Password must contain at least one special symbol' })
     confirmPassword: string;
 
     @IsString()
