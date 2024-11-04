17da266fb608dd503aa9a99e7b923533b5946f02
Yurii Soroka <urasor04@gmail.com>
2024-11-04 14:27:39 +0200

    fix: added endpoints for deleting users/admins

--- src/auth/admin/admin.controller.ts
+++ src/auth/admin/admin.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Request, UseGuards, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, Request, UseGuards, BadRequestException, Param, Delete } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { JwtAuthGuard } from '../strategies/jwt-auth.guard';
 import { RegisterAdminDto } from './dto/admin-register.dto';
+import { AdminPasswordDto } from './dto/admin-password.dto';
 
 @Controller('admin')
 export class AdminController {
@@ -13,5 +14,19 @@ export class AdminController {
   @Post('register-admin')
   async registerAdmin(@Body() registerAdminDto: RegisterAdminDto, @Request() req) {
     return this.adminService.registerAdmin(registerAdminDto, req.user.sub);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('delete-user/:id')
+  async deleteUser(@Request() req, @Param('id') userId: string) {
+   const parsedUserId = parseInt(userId, 10)
+    return this.adminService.deleteUser(req.user.sub, parsedUserId)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('delete-admin/:id') 
+  async deleteAdmin(@Request() req, @Param('id') adminToDeleteId: string, @Body() adminPasswordDto: AdminPasswordDto) {
+    const parsedAdminToDeleteId = parseInt(adminToDeleteId, 10);
+    return this.adminService.deleteAdmin(adminPasswordDto.adminPassword, req.user.sub, parsedAdminToDeleteId)
   }
 }
--- src/auth/admin/admin.service.ts
+++ src/auth/admin/admin.service.ts
@@ -4,6 +4,7 @@ import * as bcrypt from 'bcrypt';
 import { RegisterAdminDto } from './dto/admin-register.dto';
 import { JwtService } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
+import { UserService } from 'src/user/user.service';
 
 dotenv.config();
 
@@ -12,8 +13,8 @@ export class AdminService {
   constructor(
     private prisma: PrismaService,
     private jwtService: JwtService,
+    private userService: UserService
   ) {}
-
   async registerAdmin(registerAdminDto: RegisterAdminDto, userId: number) {
     const user = await this.prisma.user.findUnique({ where: { id: userId } });
   
@@ -57,4 +58,49 @@ export class AdminService {
       },
     });
   }  
+
+  async deleteUser(adminId: number, userId: number) {
+    const admin = await this.userService.findUserById(adminId);
+  
+    if (!admin || admin.roleId !== 2) {
+      throw new NotFoundException('Only admins can delete users');
+    }
+    const user = await this.userService.findUserById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found!');
+    }
+
+    await this.prisma.user.delete({
+      where: { id: userId },
+  });
+  return { message: 'User has been deleted successfully!' };
+}
+
+  async deleteAdmin(adminPass: string, adminId: number, adminToDeleteId: number) {
+    const admin = await this.userService.findUserById(adminId);
+    const adminToDelete = await this.userService.findUserById(adminToDeleteId);
+
+    if (!admin || admin.roleId !== 2) {
+      throw new NotFoundException('Only admins can delete admins');
+    }
+
+    if (adminPass !== process.env.ADMIN_PASSWORD) {
+      throw new BadRequestException('Invalid admin password');
+  }
+
+    
+    if (adminToDelete.roleId !== 2) {
+      throw new BadRequestException('User is not admin');
+    }
+
+    if (!adminToDelete) {
+      throw new NotFoundException('Admin not found!');
+    }
+
+    await this.prisma.user.delete({
+      where: {id : adminToDeleteId},
+    });
+
+    return {message: 'Admin has been deleted succesfully!'};
+  }
 }
